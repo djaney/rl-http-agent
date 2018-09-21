80f7bc0d10209522659cc95e460efb33a1d5f7c3
Djane Rey Mabelin <d.mabelin@arcanys.com>
2018-09-21 13:58:37 +0800

    updates

--- optimizer.py
+++ optimizer.py
@@ -1,5 +1,30 @@
 #!/usr/bin/env python3
-
 import time
+import filelock
+from core.model_factory import get_agent, WEIGHTS, LOCK
+import os.path
+from filelock import Timeout, FileLock
 
-while True: time.sleep(1)
+
+def main():
+
+    lock = FileLock(LOCK)
+
+    model = get_agent()
+    with lock:
+        if os.path.isfile(WEIGHTS) and os.access(WEIGHTS, os.W_OK & os.R_OK):
+            print("weights loaded")
+            model.load_weights(WEIGHTS)
+        else:
+            print("weights saved")
+            model.save_weights(WEIGHTS)
+        os.chmod(WEIGHTS, 0o777)
+        os.chmod(LOCK, 0o777)
+
+    while True:
+        print('tick')
+        time.sleep(1)
+
+print(__name__)
+if __name__ == "__main__":
+    main()
