5e3a7475ce6020182c6ce546c34628d128e9fefe
Djane Rey Mabelin <d.mabelin@arcanys.com>
2018-09-21 19:44:25 +0800

    updates

--- api.py
+++ api.py
@@ -1,10 +1,30 @@
 #!/usr/bin/env python3
-
 import time
-
 from flask import Flask
+from filelock import FileLock
+import os.path
+
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
+dir_path = os.path.dirname(os.path.realpath(__file__))
+weights_path = "{}/{}".format(dir_path, WEIGHTS)
+lock_path = "{}/{}".format(dir_path, LOCK)
+
+lock = FileLock(lock_path)
+agent = get_agent()
+
+@app.route("/forward")
+def step(methods=['POST']):
+    """get next step"""
     return "Hello World!"
+
+@app.route("/backward")
+def reward(methods=['POST']):
+    """set reward"""
+    return "Hello World!"
+
+def load_weights():
+    with lock:
+        if os.path.isfile(weights_path) and os.access(os.R_OK):
+            agent.load_weights(weights_path)
--- optimizer.py
+++ optimizer.py
@@ -3,7 +3,7 @@ import time
 import filelock
 from core.model_factory import get_agent, WEIGHTS, LOCK
 import os.path
-from filelock import Timeout, FileLock
+from filelock import FileLock
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 weights_path = "{}/{}".format(dir_path, WEIGHTS)
@@ -12,26 +12,21 @@ lock_path = "{}/{}".format(dir_path, LOCK)
 def main():
 
     lock = FileLock(lock_path)
-
-    model = get_agent()
+    agent = get_agent()
     
     while True:
 
         with lock:
             os.chmod(lock_path, 0o777)
             if os.path.isfile(weights_path) and os.access(weights_path, os.W_OK & os.R_OK):
-                print("weights loaded")
-                model.load_weights(weights_path)
+                agent.load_weights(weights_path)
             else:
-                print("weights created")
-                model.save_weights(weights_path)
+                agent.save_weights(weights_path)
             
             time.sleep(1)
-            print('training')
 
-            model.save_weights(weights_path, overwrite=True)
+            agent.save_weights(weights_path, overwrite=True)
             os.chmod(weights_path, 0o777)
-            print("weights updated")
         time.sleep(1)
         
 if __name__ == "__main__":
