054354207dbe243b073aa52126855ddef45b7bc8
Djane Rey Mabelin <d.mabelin@arcanys.com>
2018-10-05 09:26:56 +0800

    repaet

--- test.py
+++ test.py
@@ -9,12 +9,28 @@ env = gym.make(ENV_NAME)
 for sess_id in range(1000):
     ob = env.reset()
     total_reward = 0
+    counter = 0
+    current_action = None
     while True:
-        r = requests.post('http://localhost:5000/forward', json={"state": ob.tolist()})
-        r_obj = r.json()
-        ob, reward, done, info = env.step(r_obj['action'])
+        # repeat actions for 4 frames
+        if counter >= 4 or current_action is None:
+            r = requests.post('http://localhost:5000/forward', json={"state": ob.tolist()})
+            r_obj = r.json()
+            action = r_obj['action']
+
+            ob, reward, done, info = env.step(action)
+            requests.post('http://localhost:5000/backward',
+                          json={"id": r_obj['id'], "reward": reward, "done": done, "session_id": str(sess_id)})
+            current_action = action
+            counter = 0
+        else:
+            action = current_action
+
+            ob, reward, done, info = env.step(action)
+
+            counter += 1
         env.render()
-        requests.post('http://localhost:5000/backward', json={"id": r_obj['id'], "reward": reward, "done": done, "session_id": str(sess_id)})
+
 
         total_reward += reward
 
