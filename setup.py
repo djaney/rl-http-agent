e2a3c1df58708dec462b36fc35474fc4bc4f6195
Djane Rey Mabelin <d.mabelin@arcanys.com>
2018-10-08 19:23:10 +0800

    create package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='core',
+      version='0.1',
+      description='Create HTTP interface for keras-rl to allow env using 3rd party sofware ',
+      url='https://github.com/djaney/core',
+      author='Djane Rey Mabelin',
+      author_email='thedjaney@gmail.com',
+      license='MIT',
+      packages=['core'],
+      zip_safe=False)
