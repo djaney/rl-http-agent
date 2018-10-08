ea9f48fa64c5cab15c1a3ff22b1307348c32e158
Djane Rey Mabelin <d.mabelin@arcanys.com>
2018-10-08 19:44:04 +0800

    create package

---
+++ example/client_cartpole.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import gym
+import requests
+
+ENV_NAME = 'CartPole-v0'
+env = gym.make(ENV_NAME)
+
+ob = env.reset()
+total_reward = 0
+while True:
+    r = requests.get('http://localhost:5000/get_status')
+    status = r.text.strip()
+    if status == "STATUS_DONE":
+        requests.get('http://localhost:5000/start')
+    elif status == "STATUS_WAITING_FOR_RESET_OBSERVATIONS":
+        ob = env.reset()
+        requests.post('http://localhost:5000/send_reset', json=ob.tolist())
+    elif status == "STATUS_WAITING_FOR_ACTION_RESULT":
+        r = requests.get('http://localhost:5000/get_action')
+        action = int(r.text.strip())
+        ob, reward, done, info = env.step(action)
+        total_reward += reward
+        requests.post('http://localhost:5000/send_step_result', json=[ob.tolist(), reward, done, info])
+        if done:
+            print("Total Reward: ", total_reward)
+            total_reward = 0
+            ob = env.reset()
+            requests.post('http://localhost:5000/send_reset', json=ob.tolist())
+
+    env.render()
---
+++ readme.md
@@ -0,0 +1,7 @@
+## How to use
+1. Copy contents of `example` into a new project
+1. docker-compose build
+1. `APP=example_cartpole.py docker-compose up`
+
+`example_cartpole.py` is the model.
+`client_cartpole.py` is the third party client. It can be any application
